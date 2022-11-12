0e79c45f83e0a0753c8af2be4aba70aa9d496cf9
Davide Filosa <davideclaypool@gmail.com!>
2022-11-12 13:04:20 +0100

    Fixed some bugs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+import time
 
 st.header('Weather Forecast for the next days')
 st.text_input('City', key='city')
@@ -12,21 +13,26 @@ fc_type = st.session_state['type']
 st.subheader(f'{fc_type} for the next {days if days != 1 else ""} {"day" if days == 1 else "days" } in {city}')
 
 if city:
-    filtered_data = get_data(city, days)
+    try:
+        filtered_data = get_data(city, days)
 
-    if fc_type == 'Temperature':
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        dates = [dict['dt_txt'] for dict in filtered_data]
-        labels = {'x': 'Date', 'y': 'Temperature (C)'}
-        figure = px.line(x=dates, y=temperatures, labels=labels)
-        st.plotly_chart(figure)
+        if fc_type == 'Temperature':
+            temperatures = [round(dict['main']['temp']/10,0) for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            labels = {'x': 'Date', 'y': 'Temperature (C)'}
+            figure = px.line(x=dates, y=temperatures, labels=labels)
+            st.plotly_chart(figure)
 
-    if fc_type == 'Sky':
-        sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
-        images = {'Clear': 'images/clear.png',
-                  'Clouds': 'images/cloud.png',
-                  'Rain': 'images/rain.png',
-                  'Sun': 'images/sun.png'}
-        images_path = [images[condition] for condition in sky_condition]
+        if fc_type == 'Sky':
+            sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {'Clear': 'images/clear.png',
+                      'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png',
+                      'Sun': 'images/sun.png'}
+            images_path = [images[condition] for condition in sky_condition]
 
-        st.image(images_path, width=150)
+            st.image(images_path, width=150)
+    except KeyError:
+        warning = st.error('You selected a city that does not exist. Please choose a different city.')
+        time.sleep(5)
+        warning.empty()
