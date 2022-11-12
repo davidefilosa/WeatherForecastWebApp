662a5c97ba18717bc9457eaed4c7b82476eca804
Davide Filosa <davideclaypool@gmail.com!>
2022-11-12 11:19:54 +0100

    Initial commit - Created user interface with streamlit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What this app does?
+
+The app shows a forecast of the weather for a chosen city up to the next 5 days
---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('Weather Forecast for the next days')
+st.text_input('City', key='city')
+st.slider('Forecast Days', min_value=1, max_value=5, key='days', help='Select the number of forecasted days')
+st.selectbox('Select data to show', options=['Temperature', 'Sky'], key='type', help='Choose if you want to see the forecast for the temperature or for the sky')
+city = st.session_state['city']
+days = st.session_state['days']
+type = st.session_state['type']
+st.subheader(f'{type} for the next {days} days in {city}')
